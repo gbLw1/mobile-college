c34754ad2bebda5ddf720f84cb9ad043419c3008
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2023-09-02 12:45:56 -0300

    feat: Jogo da velha

--- Projeto2/src/JogoDaVelha/MainPage.xaml.cs
+++ Projeto2/src/JogoDaVelha/MainPage.xaml.cs
@@ -2,23 +2,118 @@
 
 public partial class MainPage : ContentPage
 {
-    int count = 0;
+    Player Player { get; set; } = Player.X;
 
-    public MainPage()
+    public MainPage() => InitializeComponent();
+
+    async void HandleClick(object sender, EventArgs e)
     {
-        InitializeComponent();
+        Button btn = (Button)sender;
+
+        btn.IsEnabled = false;
+
+        if (Player is Player.X)
+        {
+            btn.Text = "X";
+            Player = Player.O;
+        }
+        else
+        {
+            btn.Text = "O";
+            Player = Player.X;
+        }
+
+        if (VerificarVencedor(Player.X))
+        {
+            await DisplayAlert("GG", "O jogador ❌ venceu!", "Jogar novamente.");
+            JogarNovamente();
+        }
+        else if (VerificarVencedor(Player.O))
+        {
+            await DisplayAlert("GG", "O jogador ⭕ venceu!", "Jogar novamente.");
+            JogarNovamente();
+        }
+        else
+        {
+            await VerificarVelha();
+        }
+
     }
 
-    private void OnCounterClicked(object sender, EventArgs e)
+    async Task VerificarVelha()
     {
-        count++;
+        if (!string.IsNullOrWhiteSpace(btn10.Text) && !string.IsNullOrWhiteSpace(btn11.Text) && !string.IsNullOrWhiteSpace(btn12.Text)
+            && !string.IsNullOrWhiteSpace(btn20.Text) && !string.IsNullOrWhiteSpace(btn21.Text) && !string.IsNullOrWhiteSpace(btn22.Text)
+            && !string.IsNullOrWhiteSpace(btn30.Text) && !string.IsNullOrWhiteSpace(btn31.Text) && !string.IsNullOrWhiteSpace(btn32.Text))
+        {
+            await DisplayAlert("GG", "Deu velha 🥸", "Jogar novamente.");
+            JogarNovamente();
+        };
+    }
 
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
+    bool VerificarVencedor(Player player)
+    {
+        // Verificar horizontais
+        if ((btn10.Text == player.ToString() && btn11.Text == player.ToString() && btn12.Text == player.ToString())
+            || (btn20.Text == player.ToString() && btn21.Text == player.ToString() && btn22.Text == player.ToString())
+            || (btn30.Text == player.ToString() && btn31.Text == player.ToString() && btn32.Text == player.ToString()))
+        {
+            return true;
+        }
 
-        SemanticScreenReader.Announce(CounterBtn.Text);
+        // verificar verticais
+        if ((btn10.Text == player.ToString() && btn20.Text == player.ToString() && btn30.Text == player.ToString())
+            || (btn11.Text == player.ToString() && btn21.Text == player.ToString() && btn31.Text == player.ToString())
+            || (btn12.Text == player.ToString() && btn22.Text == player.ToString() && btn32.Text == player.ToString()))
+        {
+            return true;
+        }
+
+        // verificar diagonais
+        if ((btn10.Text == player.ToString() && btn21.Text == player.ToString() && btn32.Text == player.ToString())
+            || (btn12.Text == player.ToString() && btn21.Text == player.ToString() && btn30.Text == player.ToString()))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    void JogarNovamente()
+    {
+        btn10.Text = string.Empty;
+        btn10.IsEnabled = true;
+
+        btn11.Text = string.Empty;
+        btn11.IsEnabled = true;
+
+        btn12.Text = string.Empty;
+        btn12.IsEnabled = true;
+
+        btn20.Text = string.Empty;
+        btn20.IsEnabled = true;
+
+        btn21.Text = string.Empty;
+        btn21.IsEnabled = true;
+
+        btn22.Text = string.Empty;
+        btn22.IsEnabled = true;
+
+        btn30.Text = string.Empty;
+        btn30.IsEnabled = true;
+
+        btn31.Text = string.Empty;
+        btn31.IsEnabled = true;
+
+        btn32.Text = string.Empty;
+        btn32.IsEnabled = true;
+
+        Player = Player.X;
     }
 }
 
+enum Player
+{
+    X = 0,
+    O = 1,
+}
--- Projeto2/src/JogoDaVelha/Platforms/Android/MainActivity.cs
+++ Projeto2/src/JogoDaVelha/Platforms/Android/MainActivity.cs
@@ -3,7 +3,23 @@ using Android.Content.PM;
 using Android.OS;
 
 namespace JogoDaVelha;
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/Maui.SplashTheme",
+    MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.ScreenSize
+                           | ConfigChanges.Orientation
+                           | ConfigChanges.UiMode
+                           | ConfigChanges.ScreenLayout
+                           | ConfigChanges.SmallestScreenSize
+                           | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        Window.ClearFlags(Android.Views.WindowManagerFlags.TranslucentStatus);
+        Window.AddFlags(Android.Views.WindowManagerFlags.LayoutNoLimits);
+        Window.AddFlags(Android.Views.WindowManagerFlags.LayoutInScreen);
+        Window.DecorView.SetFitsSystemWindows(true);
+    }
 }
