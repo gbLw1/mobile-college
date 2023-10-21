b7c1a5ee4af7e8b48ba5d0266d78adb634bc3ee2
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2023-10-21 12:32:03 -0300

    add: Atividade model

---
+++ Projeto4/src/AppFit/Models/Atividade.cs
@@ -0,0 +1,10 @@
+﻿namespace AppFit.Models;
+
+public class Atividade
+{
+    public int Id { get; set; }
+    public string Descricao { get; set; }
+    public DateTime Data { get; set; }
+    public double? Peso { get; set; }
+    public string Observacoes { get; set; }
+}
