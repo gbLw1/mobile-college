7cc1f9d07270768f424ab66f39545f6e6182ef15
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2023-10-28 14:56:01 -0300

    feat: inicializando sqlite (App)

--- Projeto4/src/AppFit/App.xaml.cs
+++ Projeto4/src/AppFit/App.xaml.cs
@@ -1,7 +1,27 @@
-﻿namespace AppFit;
+﻿using AppFit.Helpers;
+
+namespace AppFit;
 
 public partial class App : Application
 {
+    static SQLiteDatabaseHelper database;
+
+    public static SQLiteDatabaseHelper Database
+    {
+        get
+        {
+            database ??= new SQLiteDatabaseHelper(
+                Path.Combine(
+                    Environment.GetFolderPath(
+                        Environment.SpecialFolder.LocalApplicationData),
+                    "banco.db3"
+                )
+            );
+
+            return database;
+        }
+    }
+
     public App()
     {
         InitializeComponent();
