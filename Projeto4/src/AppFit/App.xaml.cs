b83ea31c01a151be42d5644b4d86eb12082aa274
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2023-10-28 21:59:02 -0300

    style: indenting

--- Projeto4/src/AppFit/App.xaml.cs
+++ Projeto4/src/AppFit/App.xaml.cs
@@ -7,13 +7,12 @@ public partial class App : Application
     static SQLiteDatabaseHelper database;
 
     public static SQLiteDatabaseHelper Database =>
-            database ??= new SQLiteDatabaseHelper(
-                Path.Combine(
-                    Environment.GetFolderPath(
-                        Environment.SpecialFolder.LocalApplicationData),
-                    "banco.db3"
-                )
-            );
+        database ??= new SQLiteDatabaseHelper(
+            Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "banco.db3"
+            )
+        );
 
     public App()
     {
